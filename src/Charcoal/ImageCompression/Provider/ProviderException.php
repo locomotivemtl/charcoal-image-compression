0ed8036de429280a71fe6c5b7bf1425f3f97885e
Joel Alphonso <joel@locomotive.ca>
2022-03-30 16:13:43 -0400

    feat: Add ProviderException

---
+++ src/Charcoal/ImageCompression/Provider/ProviderException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Charcoal\ImageCompression\Provider;
+
+/**
+ * Provider Exception
+ *
+ * Exception thrown when a compression providers fails or throws an error
+ */
+class ProviderException extends \Exception
+{
+
+}
