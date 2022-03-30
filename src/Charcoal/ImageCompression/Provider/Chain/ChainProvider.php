28a684d991490b9223c9433860b5032652b28735
Joel Alphonso <joel@locomotive.ca>
2022-03-30 18:05:34 -0400

    feat: (WIP) add a chain provider which allows chaining multiple providers when querying a compression

---
+++ src/Charcoal/ImageCompression/Provider/Chain/ChainProvider.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Charcoal\ImageCompression\Provider\Chain;
+
+use Charcoal\ImageCompression\Provider\AbstractProvider;
+use Charcoal\ImageCompression\Provider\ProviderException;
+use Charcoal\ImageCompression\Provider\ProviderInterface;
+
+/**
+ * Class ChainProvider
+ */
+class ChainProvider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * @var ProviderInterface[]
+     */
+    private array $providers;
+
+    /**
+     * @param ProviderInterface[] $providers List of providers to chain.
+     */
+    public function __construct(array $providers)
+    {
+        $this->providers = $providers;
+    }
+
+    /**
+     * @param string      $source The source file path.
+     * @param string|null $target The target file path, if empty, will overwrite the source file.
+     * @return boolean
+     * @throws ProviderException When a provider is failing.
+     */
+    public function compress(string $source, ?string $target = null): bool
+    {
+        foreach ($this->providers as $provider) {
+            try {
+                $provider->compress($source, $target);
+
+                return true;
+            } catch (ProviderException $e) {
+                // Log exception
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @return string|null
+     * @throws ProviderException When a provider is failing.
+     */
+    public function compressionCount(): ?string
+    {
+        return array_reduce($this->providers, fn($carry, $provider) => ($carry + $provider->compressionCount()));
+    }
+}
