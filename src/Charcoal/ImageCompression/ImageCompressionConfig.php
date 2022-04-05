f4c67a604f184fe55757418bf181de207fa9b867
Joel Alphonso <joel@locomotive.ca>
2022-04-05 15:37:53 -0400

    feat: add registry model to config object properties

--- src/Charcoal/ImageCompression/ImageCompressionConfig.php
+++ src/Charcoal/ImageCompression/ImageCompressionConfig.php
@@ -3,12 +3,20 @@
 namespace Charcoal\ImageCompression;
 
 use Charcoal\Config\AbstractConfig;
+use Charcoal\ImageCompression\Contract\Model\RegistryInterface;
 
 /**
  * Config: Image Compression
  */
 class ImageCompressionConfig extends AbstractConfig
 {
+    /**
+     * The model that'll serve to keep track of currently optimized images.
+     *
+     * @var string $registryObject
+     */
+    private string $registryObject;
+
     /**
      * The default data is defined in a JSON file.
      *
@@ -19,8 +27,25 @@ class ImageCompressionConfig extends AbstractConfig
         $baseDir = rtrim(realpath(__DIR__.'/../../../'), '/');
         $confDir = $baseDir.'/config';
 
-        $config = $this->loadFile($confDir.'/image-compression.json');
+        return $this->loadFile($confDir.'/image-compression.json');
+    }
 
-        return $config;
+    /**
+     * @return string
+     */
+    public function getRegistryObject(): string
+    {
+        return $this->registryObject;
+    }
+
+    /**
+     * @param string $registryObject RegistryObject for ImageCompressionConfig.
+     * @return self
+     */
+    public function setRegistryObject(string $registryObject): self
+    {
+        $this->registryObject = $registryObject;
+
+        return $this;
     }
 }
